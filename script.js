ff21ad51c50c597ef36c062c6a8322141d4b25b4
Sufiyan Ahmed <100953582+Sufiyan92@users.noreply.github.com>
2023-01-13 16:10:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+let x = str.toLowerCase();
+for(let i=0 ; i<x.length/2 ;i++ ){
+    if(x[i]!=x[x.length-i-1]){
+        return false;
+    }
+}
+return true;
 }
 module.exports = palindrome
